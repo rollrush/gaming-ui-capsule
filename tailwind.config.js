586b1d444fb8cc83bca6c2ccf5449cfe96959991
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2024-06-09 10:44:03 +0530

    dice and home updated with new UI fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+        DEFAULT: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        lg: '3px 3px 6px rgba(0, 0, 0, 0.5)',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.text-shadow-sm': {
+          textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow': {
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-lg': {
+          textShadow: '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-none': {
+          textShadow: 'none',
+        },
+      });
+    },
   ],
-}
+};
+
+
+
 
