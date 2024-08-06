211db4d5c6a6d81d9a38af360ed852e53c84ff72
lokesh-katari <lokeshkatari921@outlook.com>
2024-08-06 10:32:30 +0530

    refractored code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -120,7 +120,7 @@ function Navbar() {
               </div>
               <ul
                 tabIndex={0}
-                className="dropdown-content bg-gray-800 z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
                   <Link
